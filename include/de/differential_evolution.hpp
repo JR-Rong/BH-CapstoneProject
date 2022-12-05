3fb6b71a8cfc0d8dc0749b7204d2b99acff49b66
jianrui-rong <rongjianrui@gmail.com>
2022-12-05 17:13:01 +0800

    add class uml

---
+++ include/de/differential_evolution.hpp
@@ -0,0 +1,20 @@
+/*
+ * @Author: jianrui-rong rongjianrui@gmail.com
+ * @Date: 2022-11-29 19:56:25
+ * @LastEditors: jianrui-rong
+ * @LastEditTime: 2022-11-29 20:54:53
+ * @Description: 
+ */
+
+#include <memory>
+#include <vector>
+#include <math.h>
+
+namespace de {
+
+class differential_evolution{
+private:
+    
+};
+
+} //namespace de
